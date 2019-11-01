dc73233b464bf5ebd3141dac34cbdbfe49013d1e
Daniel Xu <daniel.xu@fleetcomplete.com>
2019-11-01 11:56:01 -0400

    Finished CRUD of admins

--- backend/schema/admins.js
+++ backend/schema/admins.js
@@ -1,15 +1,19 @@
-const { default_dbname, get, insert } = require('../database');
+const { default_dbname, get, insert, update, del } = require('../database');
 const ObjectID = require('mongodb').ObjectID;
-const uuid4 = require('uuid/v4');
 const { logger, dblogger } = require('../log/logger');
 
 module.exports = {
     query: {
         allAdmins: '[Admin]',
-        'getAdmin(id: ID!)': 'Admin'
+        'getAdminByID(id: ID!)': 'Admin',
+        'getAdminByUsername(username: String!)': 'Admin',
     },
     mutation: {
-        'addAdmin(input: AdminInput)': 'Admin'  
+        'addAdmin(input: AdminInput)': 'Admin',
+        'updateAdminByID(id: ID!, input: AdminUpdate)': 'Admin',
+        'updateAdminByUsername(username: String!, input: AdminUpdate)': 'Admin',
+        'deleteAdminByID(id: ID!)': 'Boolean',
+        'deleteAdminByUsername(username: String!)': 'Boolean'
     },
     typeDef: `
         type Admin {
@@ -31,6 +35,16 @@ module.exports = {
             email: String!
             phone: String
         }
+
+        input AdminUpdate {
+            firstname: String
+            lastname: String
+            username: String
+            password: String
+            email: String
+            phone: String
+            type: AdminType
+        }
     `,
     mutationResolver: {
         addAdmin: async function(parent, args, context, info) {
@@ -39,19 +53,74 @@ module.exports = {
             logger.info(`Added new admin, ${JSON.stringify(newAdmin)}`)
             return newAdmin;
         },
+        updateAdminByID: async function(parent, args, context, info) {
+            let adminID = args.id;
+            let newData = args.input;
+            if(newData) {
+                logger.info(`Updated admin from updateAdminByID request, id: ${adminID}, newData: ${JSON.stringify(newData)}`);
+                const result = await update(default_dbname, 'admins', {_id: ObjectID(adminID)}, {$set: newData}).value;
+                return result.value;
+            }
+            logger.info(`Update admin from updateAdminByID request skipped since newData is not provided`);
+            return null;
+        },
+        updateAdminByUsername: async function(parent, args, context, info) {
+            let username = args.username;
+            let newData = args.input;
+            if(newData) {
+                logger.info(`Updated admin from updateAdminByUsername request, username: ${username}, newData: ${JSON.stringify(newData)}`);
+                const result =  await update(default_dbname, 'admins', {username: username}, {$set: newData});
+                return result.value;
+            }
+            logger.info(`Update admin from updateAdminByUsername request skipped since newData is not provided`);
+            return null;
+        },
+        deleteAdminByID: async function(parent, args, context, info) {
+            let adminID = args.id;
+            const result = await del(default_dbname, 'admins', {_id: ObjectID(adminID)});
+            if(result.result.n === 0) {
+                logger.info(`Delete admin from deleteAdminByID request, id: ${adminID}, result: false`);
+                return false;
+            }
+            logger.info(`Delete admin from deleteAdminByID request, id: ${adminID}, result: true`);
+            return true;
+        },
+        deleteAdminByUsername: async function(parent, args, context, info) {
+            let username = args.username;
+            const result = await del(default_dbname, 'admins', {username: username});
+            if(result.result.n === 0) {
+                logger.info(`Delete admin from deleteAdminByUsername request, username: ${username}, result: false`);
+                return false;
+            }
+            logger.info(`Delete admin from deleteAdminByUsername request, username: ${username}, result: true`);
+            return true;
+        }
     },
     queryResolver: {
-        getAdmin: async function(parent, args, context, info) {
+        getAdminByID: async function(parent, args, context, info) {
             const adminID = args.id;
             const result = await get(default_dbname, 'admins', {_id: ObjectID(adminID)});
             if(result.length > 1) {
                 dblogger.error(`Duplicated ID in database: ${adminID}, # of duplicates: ${result.length}`);
             }
             if(result.length === 0) {
-                logger.info(`Return result from getAdmin request, null`);
+                logger.info(`Return result from getAdminByID request, ID: ${adminID}, result: null`);
                 return null;
             }
-            logger.info(`Return result from getAdmin request, ${JSON.stringify(result[0])}`);
+            logger.info(`Return result from getAdminByID request, ID:${adminID}, result: ${JSON.stringify(result[0])}`);
+            return result[0];
+        },
+        getAdminByUsername: async function(parent, args, context, info) {
+            const username = args.username;
+            const result = await get(default_dbname, 'admins', {username: username});
+            if(result.length > 1) {
+                dblogger.error(`Duplicated ID in database: ${adminID}, # of duplicates: ${result.length}`);
+            }
+            if(result.length === 0) {
+                logger.info(`Return result from getAdminByUsername request, username: ${username}, result: null`);
+                return null;
+            }
+            logger.info(`Return result from getAdminByUsername request, username:${username}, result: ${JSON.stringify(result[0])}`);
             return result[0];
         },
         allAdmins: async function(parent, args, context, info) {
