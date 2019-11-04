145fb1bd795486d57ac49ab934519913f3fea5e5
Daniel Xu <daniel.xu@fleetcomplete.com>
2019-11-04 13:23:43 -0500

    Added apollo client packages

--- frontend/js/index.jsx
+++ frontend/js/index.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+let BASE_URL;
+if(process.env.NODE_ENV !== 'prod') {
+    BASE_URL = 'localhost:5000/graphql';
+}
+else {
+    BASE_URL = '';
+}
+const httpLink = new HttpLink({ uri: BASE_URL });
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+    link: httpLink,
+    cache,
+});
+
+ReactDOM.render(<ApolloProvider client={client}><App/></ApolloProvider>, document.getElementById("app"));
