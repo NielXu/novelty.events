7d3e9eeec78ccc7d28010664af8975ab68781b4f
Daniel Xu <daniel.xu@fleetcomplete.com>
2019-11-05 13:26:13 -0500

    Added few pages

--- frontend/js/App.jsx
+++ frontend/js/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Home from './components/homepage';
+import Admin from './components/admin';
+import Member from './components/member';
+import Login from './components/login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default class App extends React.Component {
     constructor(props) {
@@ -33,9 +37,14 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div>
-                <Home/>
-            </div>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home}></Route>
+                    <Route exact path="/login" component={Login}></Route>
+                    <Route exact path="/admin" component={Admin}></Route>
+                    <Route exact path="/member" component={Member}></Route>
+                </Switch>
+            </Router>
         )
     }
 }
---
+++ frontend/js/components/member.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Member extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h2>Member</h2>
+    }
+}
