b043d345e8fee775c99a6773d07d969831275659
Daniel Xu <daniel.xu@fleetcomplete.com>
2019-11-05 13:13:45 -0500

    Added navbar for homepage

--- frontend/js/components/homepage.jsx
+++ frontend/js/components/homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Image } from 'react-bootstrap';
+import { Button, Image, Nav, Navbar } from 'react-bootstrap';
 import '../../css/homepage.css';
 
 export default class HomePage extends React.Component {
@@ -14,9 +14,18 @@ export default class HomePage extends React.Component {
     render() {
         return (
             <div>
-                <div className="topBar">
-                    <Button variant="primary">Login</Button>
-                </div>
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand href="#home">Novelty-UTSC</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Events</Nav.Link>
+                    </Nav>
+                    <Navbar.Collapse className="justify-content-end">
+                        {/* <Button variant="outline-primary">Login</Button> */}
+                        <Nav.Link href="#login">Login</Nav.Link>
+                    </Navbar.Collapse>
+                </Navbar>
                 <div id="homeLogoContainer">
                     <Image src={"/public/homeLogo.png"} id="homeLogo"/>
                 </div>
