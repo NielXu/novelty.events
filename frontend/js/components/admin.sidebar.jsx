c3e9292c296899a2294f2c101bf308708faac61f
Daniel Xu <daniel.xu@fleetcomplete.com>
2019-11-07 16:40:41 -0500

    Added onClick for admin sidebar to memberList page

--- frontend/js/components/admin.sidebar.jsx
+++ frontend/js/components/admin.sidebar.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
 export default class AdminSidebar extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            toMemberList: false,
+        }
+        this.onMemberListClick = this.onMemberListClick.bind(this);
+    }
+
+    onMemberListClick() {
+        this.setState({toMemberList: true});
     }
 
     render() {
@@ -13,11 +22,12 @@ export default class AdminSidebar extends React.Component {
                     <Button variant="outline-primary">Events</Button>
                 </div>
                 <div>
-                    <Button variant="outline-primary">Members</Button>
+                    <Button variant="outline-primary" onClick={this.onMemberListClick}>Members</Button>
                 </div>
                 <div>
                     <Button variant="outline-primary">Suggestions</Button>
                 </div>
+                {this.state.toMemberList && <Redirect to="/memberList"/>}
             </div>
         )
     }
