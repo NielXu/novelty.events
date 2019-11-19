2848802c35270afac7f88d01fcc38ae08d68c1f4
Daniel Xu <daniel.xu@fleetcomplete.com>
2019-11-19 11:25:55 -0500

    Added basic profile and ability to update information

--- frontend/js/components/admin.profile.jsx
+++ frontend/js/components/admin.profile.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import { toast, Slide } from 'react-toastify';
+import '../../css/admin.profile.css';
 
 export default class AdminProfile extends React.Component {
     constructor(props) {
@@ -12,7 +15,13 @@ export default class AdminProfile extends React.Component {
             lastname: '',
             email: '',
             phone: '',
+            readOnly: true,
         }
+        this.onFirstnameChange = this.onFirstnameChange.bind(this);
+        this.onLastnameChange = this.onLastnameChange.bind(this);
+        this.onEmailChange = this.onEmailChange.bind(this);
+        this.onPhoneChange = this.onPhoneChange.bind(this);
+        this.onSaveUpdateClick = this.onSaveUpdateClick.bind(this);
     }
 
     componentDidMount() {
@@ -62,6 +71,86 @@ export default class AdminProfile extends React.Component {
             this.setState({loading: false});
         })
     }
+
+    onFirstnameChange(e) {
+        this.setState({firstname: e.target.value});
+    }
+
+    onLastnameChange(e) {
+        this.setState({lastname: e.target.value});
+    }
+
+    onEmailChange(e) {
+        this.setState({email: e.target.value});
+    }
+
+    onPhoneChange(e) {
+        this.setState({phone: e.target.value});
+    }
+
+    onSaveUpdateClick() {
+        if(!this.state.firstname) {
+            this.setState({error: 'First name cannot be empty'});
+            return
+        }
+        else if(!this.state.lastname) {
+            this.setState({error: 'Last name cannot be empty'});
+            return
+        }
+        else if(!this.state.email) {
+            this.setState({error: 'Email cannot be empty'});
+            return
+        }
+        let updating = {
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+            email: this.state.email,
+        }
+        if(this.state.phone) {
+            updating['phone'] = this.state.phone;
+        }
+        fetch('/graphql', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'X-Auth-Token': localStorage.getItem('X-Auth-Token'),
+            },
+            body: JSON.stringify({
+                query: `
+                mutation {
+                    updateAdminByUsername(username: "${this.state.username}", input: {
+                        firstname: "${this.state.firstname}"
+                        lastname: "${this.state.lastname}"
+                        email: "${this.state.email}"
+                        phone: ${this.state.phone? "\""+this.state.phone+"\"" : null}
+                    }) {
+                        code
+                        message
+                    }
+                }
+                `
+            })
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            console.log(response);
+            const resp = response.data.updateAdminByUsername;
+            if(resp.code === 0) {
+                toast(`✅Successfully updated profile`, {
+                    autoClose: 3000,
+                    transition: Slide,
+                    hideProgressBar: true,
+                });
+                this.setState({readOnly: true});
+            }
+            else {
+                this.setState({error: resp.message});
+            }
+        })
+    }
     
     render() {
         if(this.state.loading) {
@@ -77,12 +166,75 @@ export default class AdminProfile extends React.Component {
         }
         return (
             <div>
-                <h2>Profile</h2>
-                <p>{this.state.username}</p>
-                <p>{this.state.firstname}</p>
-                <p>{this.state.lastname}</p>
-                <p>{this.state.email}</p>
-                <p>{this.state.phone}</p>
+                <div id="admin-profile-container">
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="basic-addon1">Username</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                            aria-label="Username"
+                            aria-describedby="basic-addon1"
+                            value={this.state.username}
+                            readOnly
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="basic-addon1">First name</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                            aria-label="First name"
+                            aria-describedby="basic-addon1"
+                            value={this.state.firstname}
+                            readOnly={this.state.readOnly}
+                            onChange={this.onFirstnameChange}
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="basic-addon1">Last name</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                            aria-label="Last name"
+                            aria-describedby="basic-addon1"
+                            value={this.state.lastname}
+                            readOnly={this.state.readOnly}
+                            onChange={this.onLastnameChange}
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="basic-addon1">Email</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                            aria-label="Email"
+                            aria-describedby="basic-addon1"
+                            value={this.state.email}
+                            readOnly={this.state.readOnly}
+                            onChange={this.onEmailChange}
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="basic-addon1">Phone</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                            aria-label="Phone"
+                            aria-describedby="basic-addon1"
+                            value={this.state.phone? this.state.phone : ""}
+                            readOnly={this.state.readOnly}
+                            onChange={this.onPhoneChange}
+                        />
+                    </InputGroup>
+                    <div id="admin-profile-lower-buttons">
+                        {this.state.readOnly? (
+                            <Button variant="outline-dark" onClick={()=>this.setState({readOnly: false})}>Update</Button>
+                        ):(
+                            <Button variant="outline-dark" onClick={this.onSaveUpdateClick}>Save</Button>
+                        )}
+                    </div>
+                    {this.state.error && <p className="error-message">{this.state.error}</p>}
+                </div>
             </div>
         )
     }
